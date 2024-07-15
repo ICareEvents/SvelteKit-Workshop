7ee3656e6a3fc1866210e90469733c4919f15818
Roopesh Ranjan <kankita32v901@gmail.com>
2024-07-15 23:32:43 +0530

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to SvelteKit Workshop</h1>
+<p class="hover:underline hover:text-red-300">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,10 @@
+
+<script lang="ts">
+    export let data;
+</script>
+
+<div>
+  {#each data.names as { name } }
+    <div class="font-semibold">{name}</div>
+  {/each}
+</div>
